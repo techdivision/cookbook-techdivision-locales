b6dacbdda5cf33a1706ed2a007cbaca6ada0df86
Robert Lemke <robert@typo3.org>
2014-01-28 15:12:37 +0100

    [TASK] Add Debian support

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rl@robertlemke.com'
 license          'MIT'
 description      'Generates and sets locales for Ubuntu / Debian servers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
 supports "ubuntu"
 supports "debian"
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 # Author:: Robert Lemke <rl@robertlemke.com>
 #
-# Copyright (c) 2013 Robert Lemke
+# Copyright (c) 2013-2014 Robert Lemke
 #
 # Licensed under the MIT License (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,16 +17,28 @@ execute "locale-gen" do
 	command "locale-gen --purge"
 end
 
-file "/var/lib/locales/supported.d/en" do
-	action :delete
-end
+case node['platform_family']
+when "debian"
+	file "/etc/locale.gen" do
+		action :create
+		owner "root"
+		group "root"
+		mode "0644"
+		content node['robertlemke-locales']['locales'].join("\n") + "\n"
+		notifies :run, "execute[locale-gen]", :immediate
+	end
+else
+	file "/var/lib/locales/supported.d/en" do
+		action :delete
+	end
 
-file "/var/lib/locales/supported.d/local" do
-	action :create
-	owner "root"
-	group "root"
-	mode "0644"
-	content node['robertlemke-locales']['locales'].join("\n") + "\n"
-	notifies :run, "execute[locale-gen]", :immediate
+	file "/var/lib/locales/supported.d/local" do
+		action :create
+		owner "root"
+		group "root"
+		mode "0644"
+		content node['robertlemke-locales']['locales'].join("\n") + "\n"
+		notifies :run, "execute[locale-gen]", :immediate
+	end
 end
 
