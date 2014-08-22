148417a654d8f48ea2fc4893d94e5a6392cff736
Robert Lemke <r.lemke@techdivision.com>
2014-08-22 12:41:08 +0200

    [TASK] Bump version to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.lemke@techdivision.com'
 license          'MIT'
 description      'Generates and sets locales for Ubuntu / Debian servers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 supports "ubuntu"
 supports "debian"
